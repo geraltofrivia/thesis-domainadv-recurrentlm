d9e2be4757cb87c32e386e5924d83c928d61edc1
geraltofrivia <pc.priyansh@gmail.com>
2018-11-06 17:18:57 +0100

    Added shell file to set things up.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # lm-transferlearning
 A codebase where we extend Language Models with our transfer learning techniques
+
+## Setup
+Run `setup.sh` after giving it executable permissions like so: `chmod +x setup.sh`.
---
+++ setup.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+echo "Shall get you up and running in no time"
+
+echo "Lets first clone mytorch. We'll be using it rather unsparingly."
+git clone https://github.com/geraltofrivia/mytorch.git -q
+pip install -r ./mytorch/requirements.txt
+
+echo "Let's clone fastai, too (and use its old stuff)"
+git clone https://github.com/fastai/fastai.git -q
+pip install -r ./fastai/requirements.txt
+mkdir tmp
+mv fastai/old/fastai/* tmp/
+rm -rf fastai/*
+mv tmp/* fastai/
+rm -rf tmp
+
+# Download IMDB
+echo "Okay time to download the raw data. \n We begin with IMDB"
+mkdir raw
+mkdir raw/imdb
+wget http://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz -P ./raw/imdb/
+
+# Other data downloads come here @TODO
+
+# Download pretrained model
+echo "Finally, lets download the pretrained model and we're good to go."
+mkdir resources
+wget -nH -r -np -P ./resources/ http://files.fast.ai/models/wt103/
